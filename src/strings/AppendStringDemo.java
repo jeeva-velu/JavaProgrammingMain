b8d7ee20b43807c0e930a9716af2e62e760601a7
jeeva-velu <jeeva7339521709@gmail.com>
2024-12-11 13:40:47 +0530

    String Apeend Operation

---
+++ src/strings/AppendStringDemo.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class AppendStringDemo {
+
+    public static void main(String[] args) {
+        String st1 = "Jeeva";
+        String st2 = "Hero";
+        String st3 = st1+st2;
+        System.out.println(st3);
+    }
+}
