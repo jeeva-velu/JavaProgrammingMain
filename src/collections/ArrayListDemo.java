c854baef02bf302621c6a7e5f2b0f38a43acc05d
jeeva-velu <jeeva7339521709@gmail.com>
2025-01-09 20:35:10 +0530

    List Interface

---
+++ src/collections/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        //Creation of objects using ArrayList class
+
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        ArrayList<Double> values = new ArrayList<Double>();
+        List<Character> characters = new ArrayList<Character>();
+        var numbers = new ArrayList<Integer>();     //Code simplicity
+
+    }
+}
